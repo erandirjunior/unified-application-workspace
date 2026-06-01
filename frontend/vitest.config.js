8ce34887b83833e0099f9b6c6fefd83aabf38a4c
erandirjunior <aefs12junior@gmail.com>
2026-06-01 19:27:10 -0300

    feat: update test configuration

--- frontend/vitest.config.js
+++ frontend/vitest.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./src/setupTests.js'],
+    setupFiles: ['./tests/setupTests.js'],
     coverage: {
       provider: 'v8',
     },
